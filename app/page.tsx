9ba4f52cb3e52f49845fe7cb7b5d3e98dac7af77
Han <59479453+recta-rrae@users.noreply.github.com>
2025-03-07 00:59:36 +0700

    feat: configure heroui, remove postcss.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
+import {Button} from '@heroui/button'; 
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. At quaerat, ratione quisquam, a magnam sapiente nostrum dolore molestias aliquid ducimus repellat nobis accusamus, distinctio dolores. Corporis, exercitationem? Perspiciatis, molestias totam?
-      Aut eius aliquam dicta id in dignissimos repudiandae recusandae perspiciatis tenetur praesentium totam molestias, beatae reprehenderit neque ea, asperiores est inventore sequi libero, quas ducimus dolorem autem harum! Saepe, nisi!
-      At ipsam sapiente culpa assumenda tempore eligendi voluptas modi corporis tenetur, nihil excepturi ipsum optio molestiae placeat, fugit molestias, vitae dolor laboriosam aperiam mollitia quia. Ab consequatur accusamus laudantium ducimus?
-      Sed asperiores sit reprehenderit provident dolore iure? Et odio voluptatem doloremque quasi perferendis expedita ducimus accusantium, iste est obcaecati accusamus, maxime aspernatur placeat, nulla illo impedit commodi consequatur reprehenderit ullam?
-      Sit maiores incidunt architecto, aut dolorem recusandae nisi explicabo accusamus ea temporibus laboriosam aperiam cupiditate deserunt quisquam magnam distinctio modi corporis iste qui sequi, blanditiis alias? Neque ipsam harum ex!
-      Expedita cumque velit quo tempora laudantium voluptatem, sequi porro, sit distinctio dolorum suscipit nam placeat aut quas perferendis deserunt amet quod assumenda doloremque. Corrupti impedit natus enim! Dicta, facere et.
-      Odit adipisci laboriosam consectetur ipsam deserunt earum! Eaque, officia? Exercitationem amet nam accusantium aliquam ducimus quo quis ea sapiente, error blanditiis vel tenetur quisquam ullam atque temporibus nisi dolor porro.
-      Quidem est dolor inventore nisi libero, pariatur animi voluptas dolore tempora sequi perspiciatis. Reprehenderit, a doloremque iusto, minima culpa corporis nihil accusantium tenetur magnam provident commodi porro dolor facere numquam.
-      Fugiat quidem necessitatibus officiis libero consectetur aperiam numquam assumenda qui aliquid esse, expedita tempore aspernatur temporibus illo error voluptates tenetur vero voluptatem recusandae nam eligendi, corporis accusantium modi ut? Quos.
-      Animi praesentium sequi, eveniet quis explicabo, laboriosam error eligendi cumque, voluptate sed laudantium laborum sint! Neque quos reiciendis ex sunt molestiae consequuntur blanditiis suscipit quas eveniet! Eum ullam molestias quasi!
+    <div>
+      <Button color='primary'>Click me</Button>
     </div>
   );
 }
