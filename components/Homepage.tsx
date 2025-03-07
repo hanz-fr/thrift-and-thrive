c76154713ca6b47398a7a63e52e06806a7785517
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-03-07 22:34:07 +0700

    update

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReviewCarousel from "./Section/ReviewSection/ReviewCarousel";
 
 // import from other components
-import HeroSection from './Section/HeroSection'
-import BestProduct from './Section/BestProduct'
-import TopBrands from './Section/TopBrands'
+import HeroSection from './Section/HeroSection/HeroSection'
+import BestProduct from './Section/BestProductSection/BestProduct'
+import TopBrands from './Section/TopBrandsSection/TopBrands'
 
 export default function Homepage() {
-  return (
-    <>
-      <ReviewCarousel />
-    </>
-  );
+    return (
+        <>
+            <HeroSection />
+
+            <BestProduct />
+
+            <TopBrands />
+        </>
+    );
 }
