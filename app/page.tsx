705bbb9305884ea7e5baa4fa98be9c824144cf8b
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-03-08 11:33:24 +0700

    feat: create wishlist page and add wishlist items

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ import React from "react";
 
 // import main component
 import Homepage from "@/components/Homepage";
+import WishlistPage from "@/components/WishlistPage";
 
 export default function Home() {
 
   return (
     <>
-      <Homepage />
+      <WishlistPage />
     </>
   );
 }
