472c977fd358672137e5b21228addf7699c9bba2
Han <59479453+recta-rrae@users.noreply.github.com>
2025-03-08 20:07:33 +0700

    fix: folder structuring, update import path

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 // import component
-import BlogFilterSection from '@/components/Organisms/Blog/BlogFilterSection'
-import BlogHeroSection from '@/components/Organisms/Blog/BlogHeroSection'
-import BlogListSection from '@/components/Organisms/Blog/BlogListSection'
+import BlogFilterSection from '@/components/Section/Blog/BlogFilterSection'
+import BlogHeroSection from '@/components/Section/Blog/BlogHeroSection'
+import BlogListSection from '@/components/Section/Blog/BlogListSection'
 
 export default function Blogpage() {
   return (
