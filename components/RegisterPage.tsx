86787a80b32864dad68f889e10ddba6655bcb8be
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-03-09 02:01:33 +0700

    feat: create register page, fix: change icon components in login page

--- components/RegisterPage.tsx
+++ components/RegisterPage.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-// import swiper react components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// import swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-// import required modules
+// Import required modules
 import { Autoplay, Pagination } from 'swiper/modules';
 
 // import components from heroui
@@ -18,9 +15,7 @@ import { Button, Input } from '@heroui/react';
 // import icons from react-icons
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
-export default function LoginPage() {
-
-    // toggle password visibility
+export default function RegisterPage() {
     const [isVisible, setIsVisible] = useState(false);
     const toggleVisibility = () => setIsVisible(!isVisible);
 
@@ -28,18 +23,54 @@ export default function LoginPage() {
         <div className='flex flex-col md:flex-row min-h-screen p-6 bg-gray-100'>
             <div className='w-full md:w-1/2 flex items-center justify-center p-6'>
                 <main className="w-full max-w-md">
-                    <h6 className='text-sm font-semibold'>Welcome back to Thrift and Thrive!</h6>
+                    <h6 className='text-sm font-semibold'>Welcome to Thrift and Thrive!</h6>
                     <h1 className="mb-6 text-2xl font-semibold text-gray-800">
-                        Login to your account
+                        Create a New Account
                     </h1>
                     <form>
                         <div className="mb-4">
                             <Input
+                                label="First Name*"
+                                placeholder="Enter your first name"
+                                type="text"
+                                className="w-full"
                                 required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <Input
+                                label="Full Name*"
+                                placeholder="Enter your full name"
+                                type="text"
+                                className="w-full"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <Input
+                                label="Username*"
+                                placeholder="Enter your username"
+                                type="text"
+                                className="w-full"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <Input
+                                label="Phone Number*"
+                                placeholder="Enter your phone number"
+                                type="number"
+                                className="w-full"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <Input
                                 label="Email*"
                                 placeholder="Enter your email"
                                 type="email"
                                 className="w-full"
+                                required
                             />
                         </div>
                         <div className="mb-4">
@@ -57,6 +88,7 @@ export default function LoginPage() {
                                         ) : (
                                             <FaEye className="text-xl text-gray-500" />
                                         )}
+
                                     </button>
                                 }
                                 label="Password*"
@@ -66,7 +98,7 @@ export default function LoginPage() {
                             />
                         </div>
                         <Button type='submit' className='w-full h-12 px-4 py-2 text-sm text-white bg-[#16423C]'>
-                            Login
+                            Register
                         </Button>
                     </form>
                 </main>
@@ -83,8 +115,8 @@ export default function LoginPage() {
                 >
                     <SwiperSlide>
                         <div className='relative'>
-                            <img src="https://img.freepik.com/free-photo/friends-shopping-second-hand-market_23-2149353748.jpg?ga=GA1.1.1396069855.1732762891&semt=ais_hybrid" alt="Carousel 1" className='h-[400px] w-full object-cover rounded-xl' />
-                            <div className="flex flex-col gap-2 absolute bottom-10 left-0 w-full bg-black bg-opacity-50 text-white text-center items-center p-2 rounded-b-lg">
+                            <img src="https://img.freepik.com/free-photo/friends-shopping-second-hand-market_23-2149353748.jpg" alt="Carousel 1" className='h-[400px] w-full object-cover rounded-xl' />
+                            <div className="absolute bottom-10 left-0 w-full bg-black bg-opacity-50 text-white text-center p-2 rounded-b-lg">
                                 <h6 className='text-lg'>Find Your Unique Style</h6>
                                 <p className='text-[12px]'>A collection of quality thrift fashion at affordable prices.</p>
                             </div>
@@ -92,8 +124,8 @@ export default function LoginPage() {
                     </SwiperSlide>
                     <SwiperSlide>
                         <div className='relative'>
-                            <img src="https://img.freepik.com/free-photo/friends-shopping-second-hand-market_23-2149353732.jpg?ga=GA1.1.1396069855.1732762891&semt=ais_hybrid" alt="Carousel 2" className='h-[400px] w-full object-cover rounded-xl' />
-                            <div className="flex flex-col gap-2 absolute bottom-10 left-0 w-full bg-black bg-opacity-50 text-white text-center items-center p-2 rounded-b-lg">
+                            <img src="https://img.freepik.com/free-photo/friends-shopping-second-hand-market_23-2149353732.jpg" alt="Carousel 2" className='h-[400px] w-full object-cover rounded-xl' />
+                            <div className="absolute bottom-10 left-0 w-full bg-black bg-opacity-50 text-white text-center p-2 rounded-b-lg">
                                 <h6 className='text-lg'>Thrifty & Stylish</h6>
                                 <p className='text-[12px]'>Shop for high-quality second-hand clothing at the best prices.</p>
                             </div>
@@ -101,8 +133,8 @@ export default function LoginPage() {
                     </SwiperSlide>
                     <SwiperSlide>
                         <div className='relative'>
-                            <img src="https://img.freepik.com/free-photo/friends-shopping-second-hand-market_23-2149353750.jpg?t=st=1741455706~exp=1741459306~hmac=9b35ca2a444df71b8d17a33ff638b57752d5e285256642c0c0e50b4e97de71d9&w=1800" alt="Carousel 3" className='h-[400px] w-full object-cover rounded-xl' />
-                            <div className="flex flex-col gap-2 absolute bottom-10 left-0 w-full bg-black bg-opacity-50 text-white text-center items-center p-2 rounded-b-lg">
+                            <img src="https://img.freepik.com/free-photo/friends-shopping-second-hand-market_23-2149353750.jpg" alt="Carousel 3" className='h-[400px] w-full object-cover rounded-xl' />
+                            <div className="absolute bottom-10 left-0 w-full bg-black bg-opacity-50 text-white text-center p-2 rounded-b-lg">
                                 <h6 className='text-lg'>Support Sustainable Fashion</h6>
                                 <p className='text-[12px]'>Be stylish without harming the environment with the best thrift options.</p>
                             </div>
@@ -111,5 +143,5 @@ export default function LoginPage() {
                 </Swiper>
             </div>
         </div>
-    )
+    );
 }
