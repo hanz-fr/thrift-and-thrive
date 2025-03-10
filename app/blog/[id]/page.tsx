cedbbd463241e6114f40da50e7d7442456be3676
MahesaSyawalA <mahesaabd4@gmail.com>
2025-03-10 23:13:45 +0700

    adjustment : blog

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 
+import React from 'react';
 import blogs from "@/BLOGS_DATA.json";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import Image from "next/image";
 
 export default function DetailBlogPage({ params }: { params: { id: string } }) {
-  const blog = blogs.find((b) => b.id === parseInt(params.id));
+  // Unwrap the params object using React.use()
+  const unwrappedParams = React.use(params);
+  const blog = blogs.find((b) => b.id === parseInt(unwrappedParams.id));
 
   // Jika blog tidak ditemukan
   if (!blog) {
@@ -18,31 +16,6 @@ export default function DetailBlogPage({ params }: { params: { id: string } }) {
 
   return (
     <div className="max-w-4xl mx-auto p-6">
-      {/* Swiper Carousel */}
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={10}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000 }}
-        className="rounded-lg overflow-hidden"
-      >
-        {blog.images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <div className="w-full h-[400px]">
-              <Image
-                src={image}
-                alt={blog.title}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-lg"
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
       {/* Judul Blog */}
       <h1 className="text-3xl font-bold mt-4">{blog.title}</h1>
 
@@ -52,7 +25,25 @@ export default function DetailBlogPage({ params }: { params: { id: string } }) {
       </p>
 
       {/* Konten Blog */}
-      <div className="mt-4 text-gray-800 leading-relaxed" dangerouslySetInnerHTML={{ __html: blog.content }}></div>
+      <div className="mt-4 text-gray-800 leading-relaxed">
+        {blog.sections.map((section, index) => (
+          <div key={index} className="mb-8">
+            {/* Gambar */}
+            <div className="w-full h-[300px] relative mb-4">
+              <Image
+                src={section.image}
+                alt={`Gambar ${index + 1}`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </div>
+
+            {/* Paragraf */}
+            <div dangerouslySetInnerHTML={{ __html: section.content }} className="text-justify" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
